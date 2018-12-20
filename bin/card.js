b5836bdcea6f0d3c3417fd4aaf54439acb99e610
Dave Furfero <furf@furf.com>
2018-12-20 11:55:19 -0800

    personalize card

--- bin/card.js
+++ bin/card.js
@@ -1,46 +1,15 @@
 #!/usr/bin/env node
-// 👆 Used to tell Node.js that this is a CLI tool
+const chalk = require('chalk');
 
-// Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
+console.log(chalk`
 
-// Define options for Boxen
-const options = {
-  padding: 1,
-  margin: 1,
-  borderStyle: 'round'
-}
 
-// Text + chalk definitions
-const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
-  labelCard: chalk.white.bold('      Card:')
-}
 
-// Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
-
-// Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
-
-console.log(chalk.green(boxen(output, options)))
+ 👋  {bold furf a.k.a. David Furfero}
+ 🌱  {rgb(187,187,187) VP Engineering at Thistle}
+ 
+ 🐦  {cyan https://twitter.com/furf}
+ 🐙  {cyan https://github.com/furf}
+ 🐝  {cyan https://linkedin.com/in/furf/}
+ 🦄  {green npx furf}
+`);
